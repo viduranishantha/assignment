efc86a97708052d6303e23b3cccbe79eb8d35067
viduranishantha <vidurar@gmail.com>
2019-10-24 17:59:36 +1100

    result list changes

--- src/properties/resultList.js
+++ src/properties/resultList.js
@@ -24,31 +24,31 @@ class ResultList extends Component {
   DelProperty = (index, e) => {
     // const newItem = SavedtData;
     // console.log(this);
-
     // const savedProperty = Object.assign([], this.state.SavedtData);
     // savedProperty.splice(index, 1);
-
     const property = Object.assign([], this.state.ResultData);
-
     property.splice(index, 1);
     this.setState({ ResultData: property });
-
     // const saved = Object.assign([], this.state.SavedtData);
     // property.splice(index, 1);
     // this.setState({ ResultData: property });
   };
 
   AddProperty = propertyId => {
+    const property = Object.assign([], this.state.SavedData);
+    property: property.concat(ResultData);
+    console.log(property);
+
+    this.setState({ SavedData: property });
+
     // const property = Object.assign([], this.state.ResultData);
     // const index = this.state.ResultData.findIndex(property => {
     //   return property.id === propertyId;
     // });
-    const activeProperty = this.state.ResultData[propertyId];
-    const resultArray = [...this.state.SavedData];
-    const newArray = [];
-    console.log(newArray);
-
-    console.log(activeProperty.concat(resultArray));
+    // const activeProperty = this.state.ResultData[propertyId];
+    // const resultArray = [...this.state.SavedData];
+    // this.setState({New});
+    // console.log(activeProperty.push(...resultArray));
   };
 
   render() {
